3ea0d20c7461e3830f0cb471b30e9197e2bd346e
TheMoonBass <pmoon543@gmail.com>
2025-09-30 20:16:01 -0500

    fixed import for sklearn

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn
+from sklearn.linear_model import LinearRegression, RidgeCV
 
 # Based on a 7:1:2 split
 TRAINING_SAMPLES = 538
@@ -97,9 +98,11 @@ final_ridge_weights_y2 = trainRidgeRegression(training_matrix, best_lambda_y2, t
 print(f'Ridge Regression MSE for Y1: {calcMeanSqError(final_ridge_weights_y1, test_matrix, test_y1)}')
 print(f'Ridge Regression MSE for Y1: {calcMeanSqError(final_ridge_weights_y2, test_matrix, test_y2)}')
 
-# Scikit-Learn for comparison
-lin_sk_model = sklearn.linear_model.LinearRegression()
-ridge_sk_model = sklearn.linear_model.RidgeCV()
+sk_lin_model_y1 = LinearRegression()
+sk_lin_model_y2 = LinearRegression()
+
+sk_ridge_model_y1 = RidgeCV()
+sk_ridge_model_y2 = RidgeCV()
 
 # Graphs and Reporting
 sns.lineplot(x=all_lambdas_y1, y=all_errors_y1, color='blue', label='Y1')
